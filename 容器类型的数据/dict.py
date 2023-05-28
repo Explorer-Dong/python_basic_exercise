7fcc2a719ae3cc6c404d953a9c4fc86da794d16f
Explore-Dong <@SUNSHINE>
2023-05-28 19:26:43 +0800

    字典练习

---
+++ 容器类型的数据/dict.py
@@ -0,0 +1,17 @@
+# @Time   : 2023-05-28 13:39
+# @File   : dict.py
+# @Author : Mr_Dwj
+
+s = input().split()
+word_cnt = {}
+for it in s:
+    if it in word_cnt:
+        word_cnt[it] += 1
+    else:
+        word_cnt[it] = 1
+
+word_list = list(word_cnt)
+word_list.sort()
+for it in word_list:
+    print(it, word_cnt[it])
+
